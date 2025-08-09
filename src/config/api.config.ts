502fed05b8783e8d8226f1c5884f61616af36264
s1mple-User <u3905173@gmail.com>
2025-08-09 18:35:53 +0500

    Update api.config.ts

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
-export const API_URL = `https://server-course-production-bfe4.up.railway.app/api`;
+export const API_URL = `https://server-course-gv0n.onrender.com/api`;
 
 export const getAuthUrl = (url: string) => `/auth/${url}`;
 export const getMailUrl = (url: string) => `/mail/${url}`;
